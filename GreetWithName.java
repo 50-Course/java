3fc233eb33c3af30847284c44760b7dc9022e4fc
50-Course <erioluwaadeodu@my.uopeople.edu>
2023-03-12 01:56:37 +0100

    added personalized greetings

---
+++ GreetWithName.java
@@ -0,0 +1,9 @@
+/**
+ * Greet the user with a personalized
+ * greetiing.
+ */
+public class GreetWithName {
+  public static void main(String[] args) {
+    System.out.println("Hello, " + args[0]);
+  }
+}
