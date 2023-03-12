fdd72568514573e9c168676bcfcfe297a547c706
50-Course <erioluwaadeodu@my.uopeople.edu>
2023-03-12 09:53:29 +0100

    :truck: corrected improper naming

---
+++ ArrayDemo.java
@@ -0,0 +1,7 @@
+// Arrays and Strings
+// Classical strings are just 
+// array of characters in Java program.
+//
+// We would be doing more than just Arrays
+// We're Dora, aboard the Xplorer and we'd
+// be exploring Data Structures in java.
